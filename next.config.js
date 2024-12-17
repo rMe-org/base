8eabb7bcd72d1f084f0737f7e2520d29e6c5f76a
Prince Mendiratta <prince@getcreatr.xyz>
2024-12-17 15:00:47 +0530

    apis

--- next.config.js
+++ next.config.js
@@ -5,6 +5,23 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'misc.scdn.co',
+			},
+			{
+				protocol: 'https',
+				hostname: 'i.scdn.co',
+			},
+			{
+				protocol: 'https',
+				hostname: 'dailymix-images.scdn.co',
+			},
+		],
+	},
+};
 
 export default config;
