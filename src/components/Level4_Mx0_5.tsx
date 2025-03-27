3e189cf099122636151fffee0f1f00cd1bfad5a5
IsomerX <dhruvbakshi12343@gmail.com>
2025-03-27 18:40:58 +0530

    hanges

--- src/components/Level4_Mx0_5.tsx
+++ src/components/Level4_Mx0_5.tsx
@@ -1,4 +1,4 @@
-'use client'; 
+"use client";
 
 import React from "react";
 import Level5_Relative_6 from "./Level5_Relative_6";
@@ -7,20 +7,17 @@ import Level5_Py7_37 from "./Level5_Py7_37";
 import Level5_Div_53 from "./Level5_Div_53";
 import Level5_Py7_354 from "./Level5_Py7_354";
 import Level5_Py7_469 from "./Level5_Py7_469";
-import Level5_Py7_524 from "./Level5_Py7_524";
-import Level5_Py7_599 from "./Level5_Py7_599";
-import Level5_Py7_713 from "./Level5_Py7_713";
-import Level5_Py7_865 from "./Level5_Py7_865";
-import Level5_Py7_918 from "./Level5_Py7_918";
-import Level5_Py7_1106 from "./Level5_Py7_1106";
-import Level5_Py7_1180 from "./Level5_Py7_1180";
-import Level5_Py7_1339 from "./Level5_Py7_1339";
-import Level5_Py7_1523 from "./Level5_Py7_1523";
-import Level5_Py7_1919 from "./Level5_Py7_1919";
-import Level5_Mt12_1936 from "./Level5_Mt12_1936";
 
 export default function Level4_Mx0_5() {
   return (
-    <div className="mx-0 w-full max-w-[1280px] px-8"><Level5_Relative_6 /><Level5_Block_33 /><Level5_Py7_37 /><Level5_Div_53 /><Level5_Py7_354 /><Level5_Py7_469 /><Level5_Py7_524 /><Level5_Py7_599 /><Level5_Py7_713 /><Level5_Py7_865 /><Level5_Py7_918 /><Level5_Py7_1106 /><Level5_Py7_1180 /><Level5_Py7_1339 /><Level5_Py7_1523 /><Level5_Py7_1919 /><Level5_Mt12_1936 /></div>
+    <div className="mx-0 w-full max-w-[1280px] px-8">
+      <Level5_Relative_6 />
+      <Level5_Block_33 />
+      <Level5_Py7_37 />
+      <Level5_Div_53 />
+      <Level5_Py7_354 />
+      <Level5_Py7_469 />
+    </div>
   );
 }
+
