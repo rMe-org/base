353fbe810b60690a63b886d7155f7e749171c95e
IsomerX <dhruvbakshi12343@gmail.com>
2025-03-27 18:29:29 +0530

    Initial commit for cloned project

--- next.config.js
+++ next.config.js
@@ -1,27 +1,27 @@
 
-      /** @type {import("next").NextConfig} */
-      const config = {
-        trailingSlash: true,
-        images: {
-          unoptimized: true,
-          remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: '*',
-              pathname: '**',
-            },
-          ],
-        },
-        eslint: {
-          ignoreDuringBuilds: true,
-        },
-        typescript: {
-          ignoreBuildErrors: true,
-        },
-        webpack: (config, { isServer }) => {
-          config.stats = "verbose";
-          return config;
-        },
-        output: "export"
-      };
-      export default config;
+/** @type {import("next").NextConfig} */
+const config = {
+  trailingSlash: true,
+	images: {
+		unoptimized: true,
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '*',
+				pathname: '**',
+			},
+		],
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	webpack: (config, { isServer }) => {
+		config.stats = "verbose";
+		return config;
+	},
+	output: "export"
+};
+export default config;
